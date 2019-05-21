4b800a1fe523acb8c6ea9608763f0c249170aa9e
LauraBejan <bejan_laura@yahoo.com>
2019-05-21 22:50:51 +0300

    Connected db

---
+++ OnlineShop/OnlineShop/Connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+
+namespace OnlineShop
+{
+    class Connection
+    {
+        public Connection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDataBase"].ConnectionString;
+
+            OracleConnection _connection = new OracleConnection();
+            _connection.ConnectionString = connectionString;
+            _connection.Open();
+            string sql = "select * from image";
+
+            OracleCommand command = new OracleCommand(sql, _connection);
+            Console.WriteLine(command);
+            _connection.Close();
+        }
+    }
+}
